4feb7157095e721960773fb292615d98f8444e61
Murphy <louismurphy360@gmail.com>
2021-08-27 10:39:49 +0300

    Navbar update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,20 @@
                 <span class="font-weight-bold">LIST</span>
             </v-toolbar-title>
             <v-spacer></v-spacer>
+    <v-menu>
+        <template v-slot:avtivator="{on, attrs}">
+            <v-btn  v-bind="attrs" v-on=on>
+                <v-icon left >expand_more</v-icon>
+                <span>Menu</span>
+            </v-btn>
+        </template>
+        <v-list>
+            <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                <v-list-item-title>{{link.text}}</v-list-item-title>
+            </v-list-item>
+        </v-list>
+    </v-menu>
+
             <v-btn text color="grey">
                 <span>Sing Out</span>
                 <v-icon>exit_to_app</v-icon>
